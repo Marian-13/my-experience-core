2d9ebb630863dc16563b94d3b006f5f2dda4f3cd
Marian-13 <marian144519@gmail.com>
2019-03-20 01:12:47 +0200

    Update root_path specs

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,22 +4,11 @@ RSpec.describe ApplicationController, type: :controller do
   describe '#index', js: true do
     render_views
 
-    context 'when user is logged in' do
-      it 'renders `front` app with its relative bundle' do
-        get :index
+    it 'renders `front` app with its relative bundle' do
+      get :index
 
-        expect(response.body).to match('<div data-react-class=\"front/App\"')
-        expect(response.body).to match('<script src=\"/packs-test/js/front-')
-      end
-    end
-
-    context 'when user is not logged in' do
-      it 'renders `landing` app with its relative bundle' do
-        get :index
-
-        expect(response.body).to match('<div data-react-class=\"landing/App\"')
-        expect(response.body).to match('<script src=\"/packs-test/js/landing-')
-      end
+      expect(response.body).to match('<div data-react-class=\"App\"')
+      expect(response.body).to match('<script src=\"/packs-test/js/front-')
     end
   end
 end
